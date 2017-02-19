b64e0e00223f5c3098b6e4f07ed91251db7b6b1d
aaron-crosbie <aaroncrosbie1994@gmail.com>
2017-02-19 19:53:10 +0000

    Adding Database Manager

--- SPC.v5/phpAssingment.v6/app/DbManager.php
+++ SPC.v5/phpAssingment.v6/app/DbManager.php
@@ -32,9 +32,44 @@ class DbManager {
     }
 
     function addUser($username, $fname, $mobile, $email, $dob, $addL1, $addL2, $addL3, $pass){
-        $sql = "INSERT INTO user (username, firstname, tel, email, dob, address1, address2, address3, pass)
-        VALUES ('$_POST[username]','$_POST[name1]','$_POST[tel]','$_POST[email]','$_POST[date]','$_POST[addresslineone]','$_POST[addresslinetwo]','$_POST[addresslinethree]','$_POST[password]')";
+        $sql = "INSERT INTO users (username, firstname, tel, email, dob, address1, address2, address3, pass) VALUES ('$username','$fname','$mobile','$email','$dob','$addL1','$addL2','$addL3','$pass')";
+        echo "$sql";
         mysqli_query($this->con, $sql);
     }
 
+    //Getters and Setters for the Users DB
+
+    function setFirstName($fname, $email){
+        $sql = "UPDATE users SET firstname= $fname WHERE email = $email";
+        mysqli_query($this->con, $sql);
+    }
+
+    function setUsername($username, $email){
+        $sql = "UPDATE users SET username= $username WHERE email = $email";
+        mysqli_query($this->con, $sql);
+    }
+
+    function setPassword($password, $email){
+        $sql = "UPDATE users SET pass = $password WHERE  email = $email";
+        mysqli_query($this->con, $sql);
+    }
+
+    function getUsername($email){
+        $sql = "SELECT username FROM users WHERE email = $email";
+        $username = mysqli_query($this->con, $sql);
+        return $username;
+    }
+
+    function getFirstName($email){
+        $sql = "SELECT firstname FROM users WHERE email = $email";
+        $fname= mysqli_query($this->con, $sql);
+        return $fname;
+    }
+
+    //End of Getters and Setters for Users DB
+
 }
+
+//Code to put unto registering.php which will register a user with the DB
+//  $dbm = new \itb\DbManager();
+//  $dbm->addUser($_POST['username'],$_POST['name1'],$_POST['tel'],$_POST['email'],$_POST['date'],$_POST['addresslineone'],$_POST['addresslinetwo'],$_POST['addresslinethree'],$_POST['password']);
